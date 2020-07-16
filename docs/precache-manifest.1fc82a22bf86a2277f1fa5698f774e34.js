ac6b75237bd434f71084e50d8c43c722b82d05f0
Boris Trochez <boris.trochez@gmail.com>
2020-07-15 21:13:52 -0500

    Docs added

---
+++ docs/precache-manifest.1fc82a22bf86a2277f1fa5698f774e34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b0b87ed0ab63ae9331dbc8146caa4b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8662a16aa7caae3be5c4",
+    "url": "/static/css/main.9496c23c.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8662a16aa7caae3be5c4",
+    "url": "/static/js/main.674d6556.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
